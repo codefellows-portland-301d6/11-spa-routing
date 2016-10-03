9ae70e23b146bd80f8049e4e8b2847ee43ee8bb2
Waxhoya <waxhoya@gmail.com>
2016-10-03 13:14:03 -0700

    added page calls to articleController and aboutController.

--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -1,6 +1,14 @@
+
+
+page('/about', aboutController);
+page('/article', articleController);
+// page('/admin', adminController);
+
 /* TODO: Configure our two routes for this app with page.js,
-    by registering each URL your app can handle, linked to a
-    single controller function to handle it: */
+by registering each URL your app can handle;, linked to a
+single controller function to handle it: */
+
+
 
 /* TODO: --DONE!-- What function do we call to activate page.js?
     Fire it off now, to execute it: */
