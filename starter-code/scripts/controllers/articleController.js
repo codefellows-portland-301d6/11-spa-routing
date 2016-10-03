9e3367424c6fdd9768735e2e932d71245adf51e9
willnickerson <wnickers@willamette.edu>
2016-10-03 13:48:20 -0700

    Configured routes, removed navHandler function from articleView.js, completed article reveal function.

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -3,6 +3,8 @@
 
   articleController.reveal = function() {
     /* TODO: Reveal only the articles section! */
+    $('.tab-content').hide();
+    $('#articles').show();
   };
 
   module.articleController = articleController;
--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -1,6 +1,18 @@
 /* TODO: Configure our two routes for this app with page.js,
     by registering each URL your app can handle, linked to a
     single controller function to handle it: */
+page('/', firstRoute);
+page('/about', aboutRoute);
+
+function firstRoute() {
+  console.log('FIRST ROUTE');
+  articleController.reveal();
+}
+
+function aboutRoute() {
+  console.log('ABOUT ROUTE');
+  aboutController.reveal();
+}
 
 /* TODO: --DONE!-- What function do we call to activate page.js?
     Fire it off now, to execute it: */
